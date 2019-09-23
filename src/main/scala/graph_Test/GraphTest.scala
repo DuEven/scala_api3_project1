d5b0e2a2b2f7cb7ae1105c8224918ff04ce0829f
duyw <1650776224@qq.com>
2019-09-23 16:07:40 +0800

    updata0923

--- src/main/scala/graph_Test/GraphTest.scala
+++ src/main/scala/graph_Test/GraphTest.scala
@@ -1,13 +1,14 @@
 package graph_Test
 
-import org.apache.spark.graphx.Edge
+import org.apache.spark.graphx.{Edge, Graph, VertexId, VertexRDD}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 /**
   * @author Duyw
   *         Date:2019/09/23  10:36:26
   * @Version ：1.0
-  * @description:
+  * @description: 图计算案例（好友关联推荐）
   *
   */
 object GraphTest {
@@ -16,38 +17,45 @@ object GraphTest {
 
     //创建点和边
     //创建点的集合
-    spark.sparkContext.makeRDD(Seq(
-      (1L,("小鼠",21)),
-      (2L,("小牛",22)),
-      (3L,("小虎",23)),
-      (4L,("小兔",24)),
-      (5L,("小龙",25)),
-      (6L,("小蛇",26)),
-      (7L,("小马",27)),
-      (8L,("小羊",28)),
-      (9L,("小猴",29)),
-      (10L,("小鸡",30)),
-      (11L,("小狗",11)),
-      (12L,("小猪",11)),
+    val vertexRDD: RDD[(Long, (String, Int))] = spark.sparkContext.makeRDD(Seq(
+      (1L, ("小1", 26)),
+      (2L, ("小2", 30)),
+      (6L, ("小6", 33)),
+      (9L, ("小9", 26)),
+      (133L, ("小133", 30)),
+      (138L, ("小138", 33)),
+      (158L, ("小158", 26)),
+      (16L, ("小16", 30)),
+      (44L, ("小44", 33)),
+      (21L, ("小21", 26)),
+      (5L, ("小5", 30)),
+      (7L, ("小7", 33))
     ))
     //构造边的集合
-    spark.sparkContext.makeRDD(Seq(
+    val edgeRDD: RDD[Edge[Int]] = spark.sparkContext.makeRDD(Seq(
       Edge(1L, 133L, 0),
       Edge(2L, 133L, 0),
       Edge(6L, 133L, 0),
       Edge(9L, 133L, 0),
-      Edge(6L, 133L, 0),
-      Edge(16L, 133L, 0),
-      Edge(21L, 133L, 0),
-      Edge(44L, 133L, 0),
-      Edge(1L, 133L, 0)
+      //dEdge(6L, 138L, 0),
+      Edge(16L, 138L, 0),
+      Edge(21L, 138L, 0),
+      Edge(44L, 138L, 0),
+      Edge(5L, 158L, 0),
+      Edge(7L, 158L, 0)
     ))
 
     //构件图
+    val graph = Graph(vertexRDD,edgeRDD)
+    //取顶点  那个点最小那个就是定点
+    val vertices: VertexRDD[VertexId] = graph.connectedComponents().vertices
 
-    Graph()
-
-
+    //匹配数据
+    vertices.join(vertexRDD).map{
+      case (userId,(cnId,(name,age))) => (cnId,List((name,age)))
+    }
+      .reduceByKey(_++_)
+      .foreach(println(_))
   }
 
 }
